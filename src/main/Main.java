a360488e67ab1e67c5217d2ccccfba6114dbaa46
Ruslan Hassonov <ruslan.hassonov@gmail.com>
2019-01-05 18:06:58 +0100

    none

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,14 +16,14 @@ public class Main extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/SeaRescueGui.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("gui/SeaRescueGui.fxml"));
         primaryStage.setTitle("Sea Rescue");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-
         launch(args);
     }
 }
